80c8f03e6e25f02f0c7261e4fc84e746505f492b
“surajitsamui” <surajitsamui011@gmail.com>
2019-06-18 14:56:51 +0530

    practice interview

--- src/practiceinterview/PracticeInterview.java
+++ src/practiceinterview/PracticeInterview.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practiceinterview;
+
+import java.util.Arrays;
 
 /**
  *
@@ -12,10 +9,61 @@ package practiceinterview;
 public class PracticeInterview {
 
     /**
-     * @param args the command line arguments
+     * Counting Sort functionality *
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void sort(int[] arr) {
+        int arrayLength = arr.length;
+        if (arrayLength == 0) {
+            return;
+        }
+        /**
+         * find maximum and minimum values *
+         */
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arrayLength; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        int range = max - min + 1;
+
+        int[] count = new int[range];
+        /**
+         * initialize the occurrence of each element in the count array *
+         */
+        for (int i = 0; i < arrayLength; i++) {
+            count[arr[i] - min]++;
+        }
+        /**
+         * calculate sum of indexes *
+         */
+        for (int i = 1; i < range; i++) {
+            count[i] += count[i - 1];
+        }
+        /**
+         * modify original array according to the sum count *
+         */
+        int j = 0;
+        for (int i = 0; i < range; i++) {
+            while (j < count[i]) {
+                arr[j++] = i + min;
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+
+        int[] testArray = {10, 13, 9, 15, 7, 13};
+
+        System.out.println("Elements before applying countingsort: " + Arrays.toString(testArray));
+
+        sort(testArray);
+
+        System.out.println("Elements after applying countingsort:  " + Arrays.toString(testArray));
+
+    }
+
 }
